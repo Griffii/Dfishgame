0554fb4a8f08c034ee4cfbf8366574997c0152df
Ty Griffith <ty.griffith@protonmail.com>
2025-03-19 23:39:32 +0900

    Added dry out mechanic.

--- characters/D_salmon/salmon_player.gd
+++ characters/D_salmon/salmon_player.gd
@@ -3,26 +3,31 @@ extends CharacterBody2D
 @onready var sprite: Sprite2D = $salmon_sprite  # Changed from Sprite2d cause this shit can move now
 @onready var collision_shape_player: CollisionShape2D = $CollisionShape_Player
 @onready var water_detector: Area2D = $Water_Detector
+@onready var dry_out_overlay = $CanvasLayer/DryOutOverlay 
 @onready var swimming_state = $PlayerState/Swimming
 @onready var flopping_state = $PlayerState/Flopping
+@onready var dead_state = $PlayerState/Dead
 var current_state: PlayerState = null
 
+@export var dry_out_time: float = 20.0
 
- ####### Internal Variables ####################################################
-#var direction = Vector2.ZERO            
+ ####### Internal Variables ####################################################           
 var facing_direction: float = 1.0
 var is_on_ground = false
 var is_in_water = false
 var is_in_waterfall = false
 var is_in_current = false
 var just_exited_water = false
+var dry_timer: float = 0.0  # Tracks remaining time before death
 ################################################################################
 
 func _ready() -> void:
+	dry_timer = dry_out_time
 	change_state(flopping_state)
 
 
 func _physics_process(delta: float) -> void:
+	check_drying_status(delta)
 	# Call the process from the state machine
 	if current_state:
 		current_state.physics_process(delta)
@@ -63,3 +68,23 @@ func enter_current():
 	is_in_current = true
 func exit_current():
 	is_in_current = false
+
+func drying_out(delta):
+	if not is_in_water:
+		dry_timer -= delta  # Reduce time if out of water
+		dry_out_overlay.update_effect(dry_timer)  # Update screen effect
+		
+		if dry_timer <= 0:
+			change_state(dead_state)  # Switch to dead state
+	elif dry_timer != dry_out_time:
+		reset_drying_timer()  # Reset timer only when necessary
+
+func check_drying_status(delta):
+	if is_in_water:
+		reset_drying_timer()  # Reset timer if in water
+	else:
+		drying_out(delta)  # Start drying timer if out of water
+
+func reset_drying_timer():
+	dry_timer = dry_out_time  # Reset timer to full
+	dry_out_overlay.update_effect(dry_timer) # Reset effect
