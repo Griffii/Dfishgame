3603f2c80eb3ccac089752f1029396ee8c3f7bf2
Ty Griffith <ty.griffith@protonmail.com>
2025-04-26 09:08:43 +0900

    Minor adjustments. The loop is complete. This will be the first unpolished demo available for the game.

--- scenes/levels/the_canopy.gd
+++ scenes/levels/the_canopy.gd
@@ -1,8 +1,8 @@
 extends Level
 
 var current_checkpoint: Node2D
-var camera_right_limit = 5500
-var camera_top_limit = -4095
+var camera_right_limit = 4500
+var camera_top_limit = -3300
 
 func _ready() -> void:
 	# Set spawn node as checkpoint
@@ -17,5 +17,20 @@ func _on_kill_plane_body_entered(body: Node2D) -> void:
 
 
 func _on_end_area_body_entered(body: Node2D) -> void:
-		if body is CharacterBody2D:
+	if body is CharacterBody2D:
 		SceneManager.load_next_level(level_name, true)
+
+
+## Checkpoint Adjustment Logic
+func checkpoint01(body: Node2D):
+	if body is CharacterBody2D:
+		current_checkpoint = $Checkpoint_Container/Checkpoint_01
+func checkpoint02(body: Node2D):
+	if body is CharacterBody2D:
+		current_checkpoint = $Checkpoint_Container/Checkpoint_02
+func checkpoint03(body: Node2D):
+	if body is CharacterBody2D:
+		current_checkpoint = $Checkpoint_Container/Checkpoint_03
+func checkpoint04(body: Node2D):
+	if body is CharacterBody2D:
+		current_checkpoint = $Checkpoint_Container/Checkpoint_04
